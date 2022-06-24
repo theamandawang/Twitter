bf9b35cd5f27cdc0e6a2e66999ae4630ab8cda61
Amanda Wang <amandawang@fb.com>
2022-06-23 22:07:34 -0700

    Broken. Attempt to create a view with all functionality of original cell view, but unable to connect outlets

---
+++ twitter/Views/TweetView.h
@@ -0,0 +1,33 @@
+//
+//  TweetView.h
+//  twitter
+//
+//  Created by Amanda Wang on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tweet.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@interface TweetView : UIView
+
+
+@property (weak, nonatomic) IBOutlet UILabel *tweetUserLabel;
+@property (weak, nonatomic) IBOutlet UITextView *tweetTextTextView;
+@property (weak, nonatomic) IBOutlet UILabel *tweetUserHandleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetDateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *tweetProfileImageView;
+@property (weak, nonatomic) IBOutlet UIButton *tweetReplyButton;
+@property (weak, nonatomic) IBOutlet UIButton *tweetRetweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *tweetFavoriteButton;
+@property (weak, nonatomic) IBOutlet UIButton *tweetMessageButton;
+@property (weak, nonatomic) IBOutlet UIImageView *tweetMediaImageView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tweetMediaImageViewHeight;
+
+
+@property (nonatomic, strong) Tweet *tweet;
+- (void) refreshData;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Views/TweetView.m
@@ -0,0 +1,82 @@
+//
+//  TweetView.m
+//  twitter
+//
+//  Created by Amanda Wang on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "TweetView.h"
+#import "APIManager.h"
+#import "DateTools.h"
+@implementation TweetView
+- (IBAction)didTapRetweet:(id)sender {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    if(self.tweet.retweeted){
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        [self refreshData];
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else {
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        [self refreshData];
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+}
+- (IBAction)didTapFavorite:(id)sender {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    if(self.tweet.favorited){
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        [self refreshData];
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error unfavoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else {
+        
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        [self refreshData];
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+}
+
+- (void) refreshData {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    [self.tweetRetweetButton setTitle: [NSString stringWithFormat: @"%d", self.tweet.retweetCount] forState:UIControlStateNormal ];
+    [self.tweetFavoriteButton setTitle: [NSString stringWithFormat: @"%d", self.tweet.favoriteCount] forState:UIControlStateNormal ];
+    NSString *favoriteImageName = self.tweet.favorited ? @"favor-icon-red.png" : @"favor-icon.png";
+    [self.tweetFavoriteButton setImage: [UIImage imageNamed: favoriteImageName] forState:UIControlStateNormal];
+    NSString *retweetImageName = self.tweet.retweeted ? @"retweet-icon-green.png" : @"retweet-icon.png";
+    [self.tweetRetweetButton setImage: [UIImage imageNamed: retweetImageName] forState:UIControlStateNormal];
+}
+@end
+
+@end
